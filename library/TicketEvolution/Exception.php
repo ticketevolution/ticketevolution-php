efe0292be4fd797d993503a275ba23cc674e1669
J Cobb <j+github@teamonetickets.com>
2012-03-12 18:51:47 -0700

    Adding back in Exception.php
    Whoops! Accidentally deleted Exception.php in the last commit.

---
+++ library/TicketEvolution/Exception.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * TicketEvolution Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://github.com/ticketevolution/ticketevolution-php/blob/master/LICENSE.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@teamonetickets.com so we can send you a copy immediately.
+ *
+ * @category    TicketEvolution
+ * @package     TicketEvolution
+ * @author      J Cobb <j@teamonetickets.com>
+ * @author      Jeff Churchill <jeff@teamonetickets.com>
+ * @copyright   Copyright (c) 2011 Team One Tickets & Sports Tours, Inc. (http://www.teamonetickets.com)
+ * @license     https://github.com/ticketevolution/ticketevolution-php/blob/master/LICENSE.txt     New BSD License
+ */
+
+
+/**
+ * @category    TicketEvolution
+ * @package     TicketEvolution
+ * @copyright   Copyright (c) 2011 Team One Tickets & Sports Tours, Inc. (http://www.teamonetickets.com)
+ * @license     https://github.com/ticketevolution/ticketevolution-php/blob/master/LICENSE.txt     New BSD License
+ */
+class TicketEvolution_Exception extends Exception
+{
+}
